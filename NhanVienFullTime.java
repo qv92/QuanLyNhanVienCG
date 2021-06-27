8e6a395379712951d75f3f04776754aee2aa5080
qv92 <83738706+qv92@users.noreply.github.com>
2021-06-27 09:24:11 +0700

    Update NhanVienFullTime.java

--- NhanVienFullTime.java
+++ NhanVienFullTime.java
@@ -2,7 +2,10 @@ public class NhanVienFullTime extends NhanVienDaoTao{
     public NhanVienFullTime(String ten, int tuoi, String gioi_tinh, String sdt, String email, double luong, String nganh) {
         super(ten, tuoi, gioi_tinh, sdt, email, luong, nganh);
     }
-
+    @Override
+    public double doanh_thu() {
+        return super.doanh_thu();
+    }
     @Override
     public String toString() {
         return super.toString()+ " : NhanVienFullTime" ;
